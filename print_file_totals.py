275449c8f99343b75fdcadf7fab1e1026503c530
Ulises jimenez <me@ulisesjimenez.com>
2025-11-04 13:04:44 +0100

    latest update

--- print_file_totals.py
+++ print_file_totals.py
@@ -15,14 +15,15 @@ currency_df = pd.DataFrame(currency_conversion)
 
 def main():
     # df_pre_merge = pd.read_csv(report_mapping['GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL'], sep='\t')
-    df_pre_merge = pd.read_csv('downloaded_reports/specific_id.tsv', sep='\t')
+    df_pre_merge = pd.read_csv('downloaded_reports/orders_data.tsv', sep='\t')
     df = pd.merge(df_pre_merge, currency_df, on='currency')
     df['item_price_as_eur'] = round(df['item-price'] * df['exchange_rate'], 2)
+    df = df[df['sales-channel'] == 'Amazon.de']
     shipped = df[df['item-status'] == 'Shipped']
     unshipped = df[df['item-status'] == 'Unshipped']
-    print(f'all orders EUR: {df["item_price_as_eur"].sum()}')
-    print(f'shipped orders EUR: {shipped["item_price_as_eur"].sum()}')
-    print(f'unshipped orders EUR: {unshipped["item_price_as_eur"].sum()}')
+    print(f'all orders EUR: {df["item_price_as_eur"].sum().round(2)}')
+    print(f'shipped orders EUR: {shipped["item_price_as_eur"].sum().round(2)}')
+    print(f'unshipped orders EUR: {unshipped["item_price_as_eur"].sum().round(2)}')
 
 
 if __name__ == '__main__':
--- sp-api-draft.py
+++ sp-api-draft.py
@@ -1,110 +1,63 @@
+import json
+import time
+
 from sp_api.api import Reports, ReportsV2
 from sp_api.base.reportTypes import ReportType
 from sp_api.base.marketplaces import Marketplaces
 from datetime import datetime, timedelta, timezone
-from shipping_data_processing import main as process_amazon_fulfilled
-from utils import report_mapping, credentials
+from utils import report_mapping, credentials, requests_folder
 
 
-def get_orders_data():
-    report_name = 'GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL'
-    get_report_data(report_type=report_name,
-                    file_destination=report_mapping[report_name])
-
-
-def get_fulfilled_data():
-    report_name = 'GET_AMAZON_FULFILLED_SHIPMENTS_DATA_GENERAL'
-    get_report_data(report_type=report_name,
-                    file_destination=report_mapping[report_name])
-
-
-def get_fulfilled_returns_data():
-    report_name = 'GET_FBA_FULFILLMENT_CUSTOMER_RETURNS_DATA'
-    get_report_data(report_type=report_name,
-                    file_destination=report_mapping[report_name])
-
-
-def get_all_returns_data():
-    report_name = 'GET_FLAT_FILE_RETURNS_DATA_BY_RETURN_DATE'
-    get_report_data(report_type=report_name,
-                    file_destination=report_mapping[report_name])
-
-
-def get_settlement_data():
-    report_name = 'GET_V2_SETTLEMENT_REPORT_DATA_FLAT_FILE_V2'
-    get_report_data(report_type=report_name,
-                    file_destination=report_mapping[report_name])
-
-
-def get_fba_inventory_ledger_data():
-    report_name = 'GET_LEDGER_SUMMARY_VIEW_DATA'
-    get_report_data(report_type=report_name,
-                    file_destination=report_mapping[report_name])
-
-
-def create_reports():
-    data_start_time = datetime(2025, 10, 27, 0, 0, 0, 0).isoformat()
-    data_end_time = datetime(2025, 10, 28, 0, 0, 0, 0).isoformat()
+def create_reports() -> None:
+    data_start_time = datetime(2025, 10, 27, 0, 0, 0, 0, tzinfo=timezone.utc).isoformat()
+    data_end_time = datetime(2025, 10, 28, 0, 0, 0, 0, tzinfo=timezone.utc).isoformat()
     result = ReportsV2(credentials=credentials,
                        marketplace=Marketplaces.DE).create_report(
-        reportType=ReportType.GET_FLAT_FILE_ALL_ORDERS_DATA_BY_LAST_UPDATE_GENERAL,
+        reportType=ReportType.GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL,
         # optionally, you can set a start and end time for your report
         dataStartTime=data_start_time,
         dataEndTime=data_end_time)
-    print('stop')
+    with open(requests_folder + 'create_report_id.json', 'w') as jf:
+        json.dump(result.payload, jf)
 
-def get_specific_report(report_id: str):
-    report_id = '8972752020395'
+
+def retrieve_ordered_report(report_type: ReportType):
+    with open(report_mapping[report_type]['request_file']) as jf:
+        retrieved_response = json.load(jf)
+    if 'reportId' in retrieved_response:
+        get_report_by_id(report_id=retrieved_response['reportId'],
+                         retries=10,
+                         retry_len_in_seconds=5)
+
+
+def get_report_by_id(report_id: str, retries: int, retry_len_in_seconds: int):
     report_boss = Reports(credentials=credentials,
                           marketplace=Marketplaces.DE)
     result = report_boss.get_report(report_id)
-    document_id = result.payload['reportDocumentId']
-    report_boss.get_report_document(reportDocumentId=document_id,
-                                    download=True,
-                                    file='downloaded_reports/specific_id.tsv')
-    print('hello')
-
-
-
-def get_report_data(report_type: str,
-                    file_destination: str):
-    report_types = [report_type]
-    processing_status = ["DONE"]
-    created_since_days = 7
-    created_until_days = 6
-    created_since = (datetime.now(timezone.utc) - timedelta(days=created_since_days)).isoformat()
-    created_until = (datetime.now(timezone.utc) - timedelta(days=created_until_days)).isoformat()
-    report_boss = Reports(credentials=credentials,
-                          marketplace=Marketplaces.DE)
-    res = report_boss.get_reports(reportTypes=report_types,
-                                  processingStatuses=processing_status,
-                                  dataStartTime=created_since,
-                                  dataEndTime=created_until,
-                                  marketplaceIds=[Marketplaces.ES.marketplace_id])
-    latest_done_report = res.payload['reports'][0]
-    report_document_id = latest_done_report['reportDocumentId']
-    report_id = latest_done_report['reportId']
-    report_boss.get_report_document(reportDocumentId=report_document_id,
-                                    download=True,
-                                    file=file_destination)
+    report_status = result.payload['processingStatus']
+    for attempt in range(retries):
+        if report_status == 'DONE':
+            report_type = result.payload['reportType']
+            document_id = result.payload['reportDocumentId']
+            report_boss.get_report_document(reportDocumentId=document_id,
+                                            download=True,
+                                            file=report_mapping[report_type]['tsv'])
+            print(f'report downloaded to {report_mapping[report_type]["tsv"]}')
+            return
+        elif report_status in ['PROCESSING', 'IN_QUEUE']:
+            # have to wait before checking again
+            time.sleep(retry_len_in_seconds)
+        elif report_status in ['CANCELLED', 'FATAL']:
+            # There was some kind of error, use report id to get error if it's present
+            print('there was some kind of error')
+            raise AssertionError(f'Report generation failed for id: {report_id}')
+    raise AssertionError(
+        f'Took too long, ran out of retries with {retries} retries with a len of {retry_len_in_seconds} seconds')
 
 
 def main():
-    get_specific_report(report_id='2323')
-    # get_orders_data()
-    # print('got orders')
-    # get_fulfilled_data()  # gets EU wide order data
-    # print('got fba fulfilled')
-    # process_amazon_fulfilled()
-    # print('processed')
-    # get_fulfilled_returns_data()
-    # print('got_fba_returns')
-    # get_all_returns_data()
-    # print('got all returns')
-    # get_settlement_data()
-    # print('got all settlement')
-    # get_fba_inventory_ledger_data()
-    # print('got fba inventory ledger')
+    retrieve_ordered_report(report_type=ReportType.GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL)
+
 
 # def main():
 #     create_reports()
